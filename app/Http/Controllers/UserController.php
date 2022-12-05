aeb65df1527b57cd1396721c062e78d63e3a0f22
Diana Moran <dianacmoran27@gmail.com>
2022-12-05 08:21:36 -0400

    fix: Correccion de actualizado, creacion, y borrado de usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -85,7 +85,7 @@ class UserController extends Controller
     public function update(Request $request, $user)
     {
         $user = User::where("cedula", $user)->first();
-        if($request->form_type != "profile" && $request->role == "administrator" ){
+        if( $request->role == "graduate" || $request->role == "administrator" ){
             $rules = [
                 "nombres"=>"required|string",
                 "apellidos"=>"required|string",
